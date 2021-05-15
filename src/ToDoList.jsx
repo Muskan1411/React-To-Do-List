fa5d8d71c5c8427c234044daf3bb8734a4586955
Muskan1411 <muskan99ag@gmail.com>
2021-05-15 18:11:42 +0530

    update: add item strikethrough functionality

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Tooltip from '@material-ui/core/Tooltip';
 
 const ToDoList = (props) => {
+    const [ line, setLine ] = useState(false);
+
+    const cutTheItem = () => {
+        setLine(true);
+    }
 
     return (
         <>
             <div className='todo-style'>
-                <i onClick={() => {props.onRemoveItem(props.id)}} className='fa fa-times' aria-hidden='true' />
-                <li>{props.itemVal}</li>
+                <Tooltip title="Cut the item">
+                    <i className='fa fa-times' aria-hidden='true' onClick={cutTheItem} />
+                </Tooltip>
+                <li style={{ textDecoration: line ? 'line-through':'none'}}>{props.itemVal}</li>
+                <Tooltip title="Delete the item">
+                    <DeleteIcon onClick={() => {props.onRemoveItem(props.id)}} className='deleteIcon' />
+                </Tooltip>
             </div>
         </>
     )
